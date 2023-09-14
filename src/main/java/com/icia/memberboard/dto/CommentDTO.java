c27e7b5e469c5898510c23472d89e86d7faedf33
freamwork97 <jsy11123@naver.com>
2023-09-15 08:50:45 +0900

    04 DTO_setting

--- src/main/java/com/icia/memberboard/dto/CommentDTO.java
+++ src/main/java/com/icia/memberboard/dto/CommentDTO.java
@@ -1,4 +1,12 @@
 package com.icia.memberboard.dto;
 
+import lombok.Data;
+
+@Data
 public class CommentDTO {
+    private Long id;
+    private Long boardId;
+    private String commentWriter;
+    private String commentContents;
+    private String createdAt;
 }
